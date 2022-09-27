bfe6548df003724ec325b09846b16364d3316d48
Allan-Ngugi <allan.ngugi@student.moringaschool.com>
2022-09-27 12:09:32 +0300

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,15 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
+import Links from "./Links";
 import user from "../data/user";
-console.log(user);
-
 function App() {
-  return (
+return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+    <NavBar />
+    <Home name={user.name} city={user.city} color={user.color} />
+    <About bio={user.bio} links={user.links} />
     </div>
-  );
+);
 }
-
 export default App;
